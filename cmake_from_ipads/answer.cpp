da159fe37a4ae51a87fdd198a206a9f68f2d0c2b
J <studyjinhuang@gmail.com>
2023-01-06 02:25:49 -0800

    basic cmake from ipads

---
+++ cmake_from_ipads/answer.cpp
@@ -0,0 +1,8 @@
+#include "answer.hpp"
+
+namespace answer {
+	int find_the_ultimate_answer()
+	{
+		return 42;
+	}
+}
---
+++ cmake_from_ipads/answer.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace answer {
+	int find_the_ultimate_answer();
+}
+
+
